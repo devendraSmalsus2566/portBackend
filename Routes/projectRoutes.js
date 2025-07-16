cdd0bb28c079916e21d97ec69c0643bc9c027d34
sandeep303030 <sandeep95959590@gmail.com>
2025-07-16 07:52:08 +0530

    changes regarding the cloudinary

--- Routes/projectRoutes.js
+++ Routes/projectRoutes.js
@@ -227,24 +227,12 @@ const path = require('path');
 const fs = require('fs');
 const router = express.Router();
 const Project = require('../Models/project'); // Your Mongoose schema
+const { cloudinary,storage } = require('../cloudinary');
 
-// Ensure "uploads/" directory exists
-const uploadDir = path.join(__dirname, '..', 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
-// Multer storage setup
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + file.originalname;
-    cb(null, uniqueSuffix);
-  }
-});
+
 const upload = multer({ storage }); 
+
 
 
 // ==================== ROUTES ==================== //
